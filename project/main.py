63ee10b3e704e51af8123b97ee2d50373d1f00d9
robertoparodo <parodo.roberto@gmail.com>
2025-09-18 09:11:34 +0200

    consegna finale

--- project/main.py
+++ project/main.py
@@ -1,9 +1,11 @@
 from utils.calcoli import sottrazione, moltiplicazione, divisione
 
+
 def main():
     print(sottrazione(10, 5))
     print(moltiplicazione(5, 5))
     print(divisione(100, 5))
+
 
 if __name__ == "__main__":
     main()
--- project/utils/calcoli.py
+++ project/utils/calcoli.py
@@ -1,8 +1,10 @@
 def sottrazione(a: int, b: int) -> int:
     return a - b
 
+
 def moltiplicazione(a: int, b: int) -> int:
     return a * b
+
 
 def divisione(a: int, b: int) -> float:
     if b != 0:
